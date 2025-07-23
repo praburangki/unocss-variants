ddfdc8980474fcba5bee4d44499520ed0acbc200
praburangki <praburangki@gmail.com>
2025-07-23 08:04:38 +0800

    feat: use tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: [
+    'src/index.js',
+  ],
+  shims: true,
+  format: [
+    'cjs',
+    'esm',
+  ],
+  target: 'es2019',
+  clean: true,
+  minify: true,
+  treeshake: true,
+  noExternal: [
+    '@vinicunca/perkakas',
+    'defu',
+  ],
+});
